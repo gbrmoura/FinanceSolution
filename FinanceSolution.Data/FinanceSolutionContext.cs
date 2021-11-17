eecb7d90f7ea8096c979ce9241f2e95c165915dd
gbrextreme <gbrextreme@hotmail.com>
2021-11-17 16:09:23 -0300

    Adicionar arquivos de projeto.

---
+++ FinanceSolution.Data/FinanceSolutionContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using FinanceSolution.Data.Models;
+
+namespace FinanceSolution.Data
+{
+    public class FinanceSolutionContext : DbContext
+    {
+
+    }
+}
---
+++ FinanceSolution.Data/Models/UsuarioModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinanceSolution.Data.Models
+{
+    public class UsuarioModel
+    {
+        public Guid Codigo { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public int MyProperty { get; set; }
+    }
+}

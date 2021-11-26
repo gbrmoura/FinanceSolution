8da55a91d184ac7f02612d38a56fa314d2cc0ffa
gbrextreme <gbrextreme@hotmail.com>
2021-11-26 17:18:44 -0300

    ..

---
+++ FinanceSolution.Inteface/Pages/Authorization/Logout.cshtml.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Threading.Tasks;
+
+namespace FinanceSolution.Inteface.Pages.Authorization
+{
+    public class LogoutModel : PageModel
+    {
+        public async Task<IActionResult> OnGet()
+        {
+            await AuthenticationHttpContextExtensions.SignOutAsync(HttpContext);
+            return Redirect("/");
+        }
+    }
+}
---
+++ FinanceSolution.Inteface/Pages/PaymentMethod/Index.cshtml.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FinanceSolution.Data;
+using FinanceSolution.Data.Models;
+using FinanceSolution.Inteface.Helper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FinanceSolution.Inteface.Pages.PaymentMethod
+{
+    public class IndexModel : PageModel
+    {
+        private FinanceSolutionContext context;
+
+        public IndexModel(FinanceSolutionContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public IActionResult OnGet()
+        {
+            return Page();
+        }   
+
+        [HttpPost]
+        public async Task<JsonResult> OnPostPayment(DataTableAjaxPostModel model)
+        {
+            try
+            {
+                List<PaymentMethodModel> pagto = context.PaymentMethod
+                .Where((e) => e.IsDeleted == false)
+                .ToList();
+
+                var result = pagto.Select(x => new {
+                    Codigo = x.Id,
+                    Descricao = x.Description,
+                    Tipo = x.Type
+                });
+
+                var data = result
+                    .Skip(model.start)
+                    .Take(model.length)
+                    .ToArray();
+
+                return new JsonResult(new
+                {
+                    draw = model.draw,
+                    recordsTotal = result.Count(),
+                    recordsFiltered = result.Count(),
+                    data = result,
+                });
+            } 
+            catch (Exception e)
+            {
+                return new JsonResult(new
+                {
+                    draw = model.draw,
+                    recordsTotal = 0,
+                    recordsFiltered = 0,
+                    Exception = e,
+                });
+            }
+            
+        }
+
+    }
+}

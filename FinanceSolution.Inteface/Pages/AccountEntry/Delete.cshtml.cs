f741c31d6eeaa79d6304ed4b10049514a235c268
Gabriel Alves <gbrextreme@hotmail.com>
2022-01-31 10:24:24 -0300

    ..

--- FinanceSolution.Inteface/Pages/AccountEntry/Delete.cshtml.cs
+++ FinanceSolution.Inteface/Pages/AccountEntry/Delete.cshtml.cs
@@ -1,12 +1,45 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using FinanceSolution.Data;
+using System.Linq;
+using System;
 
 namespace FinanceSolution.Inteface.Pages.AccountEntry
 {
     public class DeleteModel : PageModel
     {
-        public void OnGet()
+        private readonly FinanceSolutionContext _context;
+
+        public DeleteModel(FinanceSolutionContext context)
         {
+            _context = context;
+        }
+
+        public IActionResult OnGet(int id)
+        {
+            try
+            {
+                var accountEntry = _context.AccountEntry
+                    .Where(e => e.Id == id && e.IsDeleted == false)
+                    .SingleOrDefault();
+
+                if (accountEntry == null)
+                {
+                    ViewData["notFound"] = true;
+                    return Page();
+                }
+
+                accountEntry.IsDeleted = true;
+                _context.SaveChanges();
+
+                ViewData["success"] = true;
+                return Page();
+            }
+            catch (Exception e)
+            {
+                ViewData["internal"] = true;
+                return Page();
+            }
         }
     }
 }
